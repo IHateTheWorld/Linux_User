7881e8c31c0fec0500abc9349d854f477954d72b
IHateTheWorld <chenchenghui456@gmail.com>
2018-10-27 18:10:31 +0800

    Add emacs config

---
+++ emacs_config/lisp/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-minimum-prefix-length 3)
+ '(company-tooltip-idle-delay 0.08)
+ '(custom-safe-themes
+   (quote
+    ("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" default)))
+ '(popwin:popup-window-position (quote right))
+ '(popwin:popup-window-width 80))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(iedit-occurrence ((t (:background "#002b36" :foreground "#d33682" :weight bold))))
+ '(js2-external-variable ((t (:foreground "dark gray")))))
